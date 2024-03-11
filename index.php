d800909b665e6fe1461fc1a01fa490a77d8f3724
jeetubhaiya5529 <jeetendrakumar5529@gmail.com>
2024-03-12 00:19:28 +0530

    Add new file

---
+++ index.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Kuch Naya - Official Website</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Css -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="nav">
+        <img src="https://eiwgew27fhz.exactdn.com/wp-content/uploads/2023/02/logo-white.svg" alt="Logo">
+        <h4>Toptracer Range</h4>
+        <h4>Golf Lesson</h4>
+        <h4>Adventure Golf</h4>
+        <h4>Coffee Shop</h4>
+        <h4>Leagues</h4>
+    </div>
+    <div id="cursor"></div>
+    <div id="cursor-blur"></div>
+    <video autoplay loop muted src="https://sidcup-golf-family.netlify.app/Assests/hero.mp4"></video>
+
+    <div id="main">
+        <div id="page1">
+            <h1>EAT. DRINK. PLAY.</h1>
+            <h2>WELCOME TO SIDCUP FAMILY GOLF!</h2>
+            <p>Sidcup Family Golf is a multipurpose golf facility located in Sidcup, South East London. Passionate about technology, player development and making golf fun and accessible to everyone.</p>
+            <div id="arrow">
+                <i class="ri-arrow-down-line"></i>
+            </div>
+        </div>
+        <div id="page2">
+            <div id="scroller">
+                <div id="scroller-in">
+                    <h4>TOPTRACER RANGE</h4>
+                    <h4>GOLF LESSON</h4>
+                    <h4>ADVENTURE GOLF</h4>
+                    <h4>COFFEE SHOP</h4>
+                    <h4>LEAGUES</h4>
+                </div>
+                <div id="scroller-in">
+                    <h4>TOPTRACER RANGE</h4>
+                    <h4>GOLF LESSON</h4>
+                    <h4>ADVENTURE GOLF</h4>
+                    <h4>COFFEE SHOP</h4>
+                    <h4>LEAGUES</h4>
+                </div>
+            </div>
+            <div id="about-us">
+                <img src="https://eiwgew27fhz.exactdn.com/wp-content/uploads/2023/02/home-about-1-768x511.jpg?strip=all&lossy=1&sharp=1&ssl=1" alt="About-img-1" class="img-fluid">
+                <div id="about-us-in">
+                    <h3>ABOUT US</h3>
+                    <p>Home to a 46-bay, multi-tier, floodlit driving range, powered by Toptracer Range technology. Complimented by a practice green and bunker, coffee shop and American Golf Store. There truly is something for everyone as we also boast two outdoor 18-hole dinosaur themed crazy golf courses.</p>
+                </div>
+                <img src="https://eiwgew27fhz.exactdn.com/wp-content/uploads/2023/02/home-about-2-768x511.jpg?strip=all&lossy=1&sharp=1&ssl=1" alt="About-img-2" class="img-fluid">
+            </div>
+            <div id="cards-container">
+                <div class="card" id="card1">
+                    <div class="overlay">
+                        <h4>TopRacer Range</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, laboriosam commodi! Eaque asperiores molestias sunt quia impedit eligendi ab aliquam.</p>
+                    </div>
+                </div>
+                <div class="card" id="card2">
+                    <div class="overlay">
+                        <h4>Adventure Golf</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, laboriosam commodi! Eaque asperiores molestias sunt quia impedit eligendi ab aliquam.</p>
+                    </div>
+                </div>
+                <div class="card" id="card3">
+                    <div class="overlay">
+                        <h4>Golf Lessons</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, laboriosam commodi! Eaque asperiores molestias sunt quia impedit eligendi ab aliquam.</p>
+                    </div>
+                </div>
+            </div>
+            <div id="green-div">
+                <img src="https://eiwgew27fhz.exactdn.com/wp-content/themes/puttosaurus/img/dots-side.svg" alt="Element">
+                <h4>SIGN UP FOR SIDCUP NEWS AND SPECIAL OFFERS STRAIGHT TO YOUR INBOX</h4>
+                <img src="https://eiwgew27fhz.exactdn.com/wp-content/themes/puttosaurus/img/dots-side.svg" alt="Element">
+            </div>
+        </div>
+        <div id="page3">
+            <p>Pleasantly surprised to discover the mini golf is open until 10pm during weekdays 😊 Always enjoy visit here, cafe does some nice goodies too 👍🏽</p>
+            <img src="https://eiwgew27fhz.exactdn.com/wp-content/themes/puttosaurus/img/quote-left.svg" id="colon1" alt="">
+            <img src="https://eiwgew27fhz.exactdn.com/wp-content/themes/puttosaurus/img/quote-right.svg" id="colon2" alt="">
+        </div> 
+        <div id="page4">
+            <h1>what are you waiting for ?</h1>
+            <div class="elem">
+                <h2>TOPTRACER  RANGE</h2>
+                <img src="https://eiwgew27fhz.exactdn.com/wp-content/uploads/2023/02/page-toptracer.jpg?strip=all&lossy=1&sharp=1&w=1152&ssl=1" alt="">
+            </div>
+            <div class="elem">
+                <h2>GOLF LESSONS</h2>
+                <img src="https://eiwgew27fhz.exactdn.com/wp-content/uploads/2023/02/page-lessons.jpg?strip=all&lossy=1&sharp=1&w=1152&ssl=1" alt="">
+            </div>
+            <div class="elem">
+                <h2>ADVENTURE GOLF</h2>
+                <img src="https://eiwgew27fhz.exactdn.com/wp-content/uploads/2023/02/page-ag.jpg?strip=all&lossy=1&sharp=1&w=1152&ssl=1" alt="">
+            </div>
+        </div>
+        <div id="footer">
+            <img src="https://eiwgew27fhz.exactdn.com/wp-content/themes/puttosaurus/img/dots-footer.svg" alt="">
+            <div id="f1">
+                <img src="https://eiwgew27fhz.exactdn.com/wp-content/uploads/2023/02/logo-white.svg" alt="">
+            </div>
+            <div id="f2">
+                <h3>toptracer ranges</h3>
+                <h3>golf lessons</h3>
+                <h3>adventure golf</h3>
+            </div>
+            <div id="f3">
+                <h3>coffee shop</h3>
+                <h3>leagues</h3>
+                <h3>contact us</h3>
+            </div>
+            <div id="f4">
+                <h4>A20, SIDCUP BYPASS <br>
+                    CHISLEHURST <br>
+                    KENT <br>
+                    BR7 6RP <br>
+                    TEL: 0208 309 0181 <br>
+                    GET DIRECTION <br>
+                </h4>    
+            </div>
+        </div>   
+    </div>
+
+
+    
+
+    <!-- JS Library for Animations -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" integrity="sha512-16esztaSRplJROstbIIdwX3N97V1+pZvV33ABoG1H2OyTttBxEGkTsoIVsiP1iaTtM8b3+hu2kB6pQ4Clr5yug==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js" integrity="sha512-Ic9xkERjyZ1xgJ5svx3y0u3xrvfT/uPkV99LBwe68xjy/mGtO+4eURHZBW2xW4SZbFrF1Tf090XqB+EVgXnVjw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="script.js"></script>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
